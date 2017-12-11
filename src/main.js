38c719ba694385251b9c174c899601666480d92a
zhugege <980961628@qq.com>
2017-12-11 18:34:40 +0800

    v1.0

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// import element-ui
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -13,3 +19,8 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
